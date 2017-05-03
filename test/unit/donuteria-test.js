918efc76787ef2ba2304c080d6e733f8dfbaa509
Morgan Fogarty <mofo37@gmail.com>
2017-05-03 16:42:40 -0700

    validation failure test passes :)

--- test/unit/donuteria-test.js
+++ test/unit/donuteria-test.js
@@ -1,12 +1,26 @@
-// const assert = require('chai').assert;
+const assert = require('chai').assert;
 const Donuteria = require('../../lib/models/donuteria');
 
-// const expectedValidation = () => { throw new Error('expected validation errors'); };
+const expectedValidation = () => { throw new Error('expected validation errors'); };
 
 describe('donuteria model', () => {
 
   it('validates a good model', () => {
     const donuteria = new Donuteria({ name: 'Downtown' });
     return donuteria.validate();
+  });
+});
+
+describe('validation failures', () => {
+
+  it.only('name is required', () => {
+    const donuteria = new Donuteria();
+    return donuteria.validate()
+      .then(expectedValidation,
+      err => {
+        const errors = err.errors;
+        assert.ok(errors.name && errors.name.kind === 'required');
+
+      });
   });
 });
