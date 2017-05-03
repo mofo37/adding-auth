eef1fffa5b8489c7e5c3e3cba6662a7219907fc4
Morgan Fogarty <mofo37@gmail.com>
2017-05-03 09:32:49 -0700

    server running

---
+++ server.js
@@ -0,0 +1,17 @@
+const app = require('./lib/app');
+const http = require('http');
+
+const connect = require('./lib/connect');
+const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/pets';
+connect(dbUri);
+
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log('server running at', server.address());
+});
+
+
+
